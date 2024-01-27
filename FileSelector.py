cf28fb153c8c701b01b98d1d253c17e67f681f9f
Daiyaan Muhammad <daiyaan.villain@gmail.com>
2024-01-27 12:30:50 +0600

    Added a file selector tool to select the image

---
+++ FileSelector.py
@@ -0,0 +1,60 @@
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+import os
+import logging
+
+
+class FileSelector:
+    """
+    A class for selecting a file from the local file system.
+
+    Attributes:
+        pwd (str): The current working directory.
+        path (list): A list of files and directories in the current working directory.
+        extension (str): The file extension of the selected file.
+        extension_list (list): A list of supported image file extensions.
+    """
+
+    pwd = os.getcwd()
+    path = os.listdir(pwd)
+    path = WordCompleter(path)
+
+    def __init__(self):
+        self.extension = ""
+        self.extension_list = [
+            "jpg",
+            "jpeg",
+            "png",
+            "bmp",
+            "gif",
+            "tiff",
+            "tif",
+            "webp",
+            "heif",
+            "hevc",
+        ]
+
+    def select_file(self):
+        """
+        Prompts the user to select a file from the local file system.
+
+        Returns:
+            str: The absolute path of the selected file.
+        """
+        while True:
+            last_dir = self.pwd.split("\\")[-1]
+            answer = prompt(f"{last_dir}: ", completer=self.path)
+
+            if answer == ".." or os.path.isdir(answer):
+                self.pwd = os.path.abspath(os.path.join(self.pwd, answer))
+                self.path = WordCompleter(os.listdir(self.pwd))
+            else:
+                break
+
+        self.extension = answer.split(".")[-1]
+
+        if self.extension not in self.extension_list:
+            logging.error("Not an image.")
+            return
+        else:
+            return os.path.abspath(answer)
